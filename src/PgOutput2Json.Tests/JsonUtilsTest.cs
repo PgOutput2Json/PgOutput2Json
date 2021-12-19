f559bef7342be962712070c08ed74c50092f2a42
enadzan <2304785+enadzan@users.noreply.github.com>
2021-12-19 12:14:41 +0100

    Add Tests project

---
+++ src/PgOutput2Json.Tests/JsonUtilsTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PgOutput2Json.Tests
+{
+    [TestClass]
+    public class JsonUtilsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
