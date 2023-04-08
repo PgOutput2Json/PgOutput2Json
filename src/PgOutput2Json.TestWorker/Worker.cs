8e130a33ba7297b87485b52ffe37d73af6e88409
Sinisa Cehajic <scehajic@playnirvana.com>
2023-04-08 22:16:35 +0200

    Make partitions and partition filter optional in TestWorker

--- src/PgOutput2Json.TestWorker/Worker.cs
+++ src/PgOutput2Json.TestWorker/Worker.cs
@@ -14,9 +14,9 @@ namespace PgOutput2Json.TestWorker
 
         private readonly ILoggerFactory _loggerFactory;
 
-        private readonly int _batchSize;
-        private readonly int _filterFrom;
-        private readonly int _filterTo;
+        private readonly int? _batchSize;
+        private readonly int? _filterFrom;
+        private readonly int? _filterTo;
         private readonly string[] _publicationNames;
         private readonly string? _connectionString;
         private readonly PartitionInfo[] _partitions;
@@ -26,29 +26,27 @@ namespace PgOutput2Json.TestWorker
             _loggerFactory = loggerFactory;
             _connectionString = configuration.GetConnectionString("PublicationDatabase");
             _publicationNames = configuration.GetSection("AppSettings:PublicationNames").Get<string[]>() ?? Array.Empty<string>();
-            _batchSize = configuration.GetSection("AppSettings:BatchSize").Get<int>();
-            _filterFrom = configuration.GetSection("AppSettings:PartitionFilter:FromInclusive").Get<int>();
-            _filterTo = configuration.GetSection("AppSettings:PartitionFilter:ToExclusive").Get<int>();
+            _batchSize = configuration.GetSection("AppSettings:BatchSize").Get<int?>();
+            _filterFrom = configuration.GetSection("AppSettings:PartitionFilter:FromInclusive").Get<int?>();
+            _filterTo = configuration.GetSection("AppSettings:PartitionFilter:ToExclusive").Get<int?>();
             _partitions = configuration.GetSection("AppSettings:Partitions").Get<PartitionInfo[]>() ?? Array.Empty<PartitionInfo>();
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             if (_connectionString == null) throw new Exception("Missing connection string");
+            if (_publicationNames.Length == 0) throw new Exception("Missing publication names");
 
             var builder = PgOutput2JsonBuilder.Create()
                 .WithLoggerFactory(_loggerFactory)
                 .WithPgConnectionString(_connectionString)
                 .WithPgPublications(_publicationNames)
-                .WithBatchSize(250)
                 .WithJsonOptions(options =>
                 {
                     //options.WriteNulls = true;
                     //options.WriteTimestamps = true;
                     //options.WriteTableNames = true;
                 })
-                .WithPartitionFilter(_filterFrom, _filterTo)
-                .WithBatchSize(_batchSize)
                 .WithMessageHandler((json, table, key, partition) =>
                 {
                     Console.WriteLine($"{table}: {json}");
@@ -62,6 +60,11 @@ namespace PgOutput2Json.TestWorker
                 //    options.EndPoints.Add("localhost:6379");
                 //})
                 ;
+
+            if (_batchSize.HasValue)
+            {
+                builder.WithBatchSize(_batchSize.Value);
+            }
 
             foreach (var partition in _partitions ?? Array.Empty<PartitionInfo>())
             {
@@ -78,6 +81,11 @@ namespace PgOutput2Json.TestWorker
                 {
                     builder.WithPgKeyColumn(partition.Table, partition.Columns);
                 }
+            }
+
+            if (_filterFrom.HasValue && _filterTo.HasValue)
+            {
+                builder.WithPartitionFilter(_filterFrom.Value, _filterTo.Value);
             }
 
             using var pgOutput2Json = builder.Build();
