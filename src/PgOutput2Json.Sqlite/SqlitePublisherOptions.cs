c80dee46ebf6ca643c930668b6a73a40269d92d0
enadzan <2304785+enadzan@users.noreply.github.com>
2025-04-26 22:15:25 +0200

    Implement adding missing columns in SqlitePublisher

--- src/PgOutput2Json.Sqlite/SqlitePublisherOptions.cs
+++ src/PgOutput2Json.Sqlite/SqlitePublisherOptions.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.Data.Sqlite;
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.Data.Sqlite;
 
 namespace PgOutput2Json.Sqlite
 {
     public class SqlitePublisherOptions
     {
         public required SqliteConnectionStringBuilder ConnectionStringBuilder { get; set; }
+        public Func<SqliteConnection, Task>? PostConnectionSetup { get; set; }
     }
 }
