b3333c2aff9abca9d819efb9d3b1e43e723c35f9
enadzan <2304785+enadzan@users.noreply.github.com>
2025-04-27 21:43:43 +0200

    Store data copy progress in the source database
    If a user wants to use data copy option, they will have to create a database named 'pg_output2json'
    in the same db cluster that is as the source of the logical replication.
    
    We will automatically create a table 'data_copy_progress' where status and last json sent
    will be kept.
    
    This is to prevent copying data for the same tables every time the listener starts.

--- src/PgOutput2Json/IMessagePublisher.cs
+++ src/PgOutput2Json/IMessagePublisher.cs
@@ -10,11 +10,6 @@ namespace PgOutput2Json
         Task ConfirmAsync(CancellationToken token);
 
         Task<ulong> GetLastPublishedWalSeq(CancellationToken token);
-
-        Task ReportDataCopyProgress(string tableName, string lastJson, CancellationToken token);
-        Task ReportDataCopyCompleted(string tableName, CancellationToken token);
-        
-        Task<DataCopyStatus> GetDataCopyStatus(string tableName, CancellationToken token);
     }
 
     public abstract class MessagePublisher : IMessagePublisher
@@ -27,39 +22,5 @@ namespace PgOutput2Json
         {
             return Task.FromResult<ulong>(0);
         }
-
-        public virtual Task ReportDataCopyProgress(string tableName, string lastJson, CancellationToken token)
-        {
-            throw new NotImplementedException();
-        }
-
-        public virtual Task ReportDataCopyCompleted(string tableName, CancellationToken token)
-        {
-            throw new NotImplementedException();
-        }
-
-        public virtual Task<DataCopyStatus> GetDataCopyStatus(string tableName, CancellationToken token)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public class DataCopyStatus
-    {
-        public bool IsCompleted { get; set; }
-        public string? LastJson { get; set; }
-
-        /// <summary>
-        /// This is optionally populated by the client app, not used by the publisher.
-        /// It allows the client do decide where to continue the copy process, based on the lastJson exported
-        /// </summary>
-        public string? AdditionalRowFilter { get; set; }
-
-        /// <summary>
-        /// This is optionally populated by the client app, not used by the publisher.
-        /// It is used in compbination with AdditionalRowFilter, and allows the client do decide where to continue the copy process, 
-        /// based on the lastJson exported
-        /// </summary>
-        public string? OrderByColumns { get; set; }
     }
 }
