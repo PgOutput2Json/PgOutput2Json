1c6ee2297ee7dc015bc73b94ee6c611ec747d7ca
enadzan <2304785+enadzan@users.noreply.github.com>
2025-05-12 21:51:42 +0200

    Implement initial version of MongoDb publisher
    - Publishes in one collection per table
    - Collections are named schema__table
    - One unique index is created per table, for the primary keys, named schema__table_pk

---
+++ src/PgOutput2Json.MongoDb/MongoDbPublisherFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace PgOutput2Json.MongoDb
+{
+    internal class MongoDbPublisherFactory : IMessagePublisherFactory
+    {
+        private readonly MongoDbPublisherOptions _options;
+
+        public MongoDbPublisherFactory(MongoDbPublisherOptions options)
+        {
+            _options = options;
+        }
+
+        public IMessagePublisher CreateMessagePublisher(ReplicationListenerOptions listenerOptions, ILoggerFactory? loggerFactory)
+        {
+            return new MongoDbPublisher(_options, loggerFactory?.CreateLogger<MongoDbPublisher>());
+        }
+    }
+}
---
+++ src/PgOutput2Json.MongoDb/MongoDbPublisherOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using MongoDB.Driver;
+
+namespace PgOutput2Json.MongoDb
+{
+    public class MongoDbPublisherOptions
+    {
+        public required MongoClientSettings ClientSettings { get; set; }
+        public required string DatabaseName { get; set; }
+
+        public Func<MongoClient, Task>? PostConnectionSetup { get; set; }
+
+    }
+}
