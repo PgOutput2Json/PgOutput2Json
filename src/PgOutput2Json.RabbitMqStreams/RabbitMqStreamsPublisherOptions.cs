0785587f6eae87b3b9ffed65505b7412f42f32cd
enadzan <2304785+enadzan@users.noreply.github.com>
2025-10-18 10:34:11 +0200

    Implement support for RabbitMq Super Streams
    By default, PK is used for hash routing, but if the user provides
    partition key fields, they will be used instead.

--- src/PgOutput2Json.RabbitMqStreams/RabbitMqStreamsPublisherOptions.cs
+++ src/PgOutput2Json.RabbitMqStreams/RabbitMqStreamsPublisherOptions.cs
@@ -1,10 +1,19 @@
 ﻿using RabbitMQ.Stream.Client;
+using RabbitMQ.Stream.Client.Reliable;
+using System;
 
 namespace PgOutput2Json.RabbitMqStreams
 {
     public class RabbitMqStreamsPublisherOptions
     {
         public string StreamName { get; set; } = "pgoutput2json";
+
+        /// <summary>
+        /// Default is false
+        /// </summary>
+        public bool WriteTableNameToMessageKey { get; set; }
+        
+        public bool IsSuperStream { get; set; }
 
         /// <summary>
         /// Number of the messages sent for each frame-send.<br/>
@@ -14,7 +23,6 @@ namespace PgOutput2Json.RabbitMqStreams
         /// </summary>
         public int MessageBufferSize { get; set; } = 100;
 
-
         public StreamSystemConfig StreamSystemConfig { get; set; } = new StreamSystemConfig
         {
             UserName = "guest",
@@ -23,6 +31,19 @@ namespace PgOutput2Json.RabbitMqStreams
             Endpoints = [],
 
             ClientProvidedName = "PgOutput2Json",
+        };
+
+        public SuperStreamConfig SuperStreamConfig { get; set; } = new SuperStreamConfig
+        {
+            Routing = msg =>
+            {
+                return msg.ApplicationProperties != null
+                    && msg.ApplicationProperties.TryGetValue(ApplicationProperties.PartitionKey, out var partitionKey)
+                    && partitionKey != null
+                    ? partitionKey.ToString()
+                    : msg.Properties?.MessageId?.ToString() ?? Guid.NewGuid().ToString();
+            },
+            RoutingStrategyType = RoutingStrategyType.Hash,
         };
     }
 }
